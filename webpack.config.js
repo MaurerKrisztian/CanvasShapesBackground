e0894693ea97e25bbf07999cddddb4d4c3130558
krisztian.maurer <krisztian.maurer@metrix.co.hu>
2022-07-25 21:31:03 +0200

    fix: fix webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.ts',
     devtool: 'inline-source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+        })
+    ],
     module: {
         rules: [
             {
@@ -19,4 +25,15 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    devServer: {
+        compress: true,
+        port: 9000,
+        open: true,
+    },
+    performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000
+    },
+    mode: 'production'
 };
